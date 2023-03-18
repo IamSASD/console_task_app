1cc67cb0ed0392160fc6c4e8216c93e4ba3a2775
Sebastian Segura <86534309+IamSASD@users.noreply.github.com>
2023-03-18 14:26:42 -0500

    Update tasks.sql
    Remove completion column and not null constrain from completion_date column

--- sql/tasks.sql
+++ sql/tasks.sql
@@ -3,6 +3,5 @@ CREATE DATABASE tasks_app;
 CREATE TABLE tasks(
     id VARCHAR(20) PRIMARY KEY NOT NULL,
     task_description VARCHAR(124) NOT NULL,
-    completion_date TIMESTAMP WITH TIME ZONE NOT NULL,
-    completed BOOLEAN
+    completion_date TIMESTAMP WITH TIME ZONE
 );
